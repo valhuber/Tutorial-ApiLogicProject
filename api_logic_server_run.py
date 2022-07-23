a9d2fa512e713831bfaf7985715b702d7df7b267
Val Huber <valjhuber@gmail.com>
2022-07-23 13:51:17 +0000

    running with cs ports

--- api_logic_server_run.py
+++ api_logic_server_run.py
@@ -2,7 +2,7 @@
 """
   ApiLogicServer v 5.03.23
 
-  Created on July 22, 2022 10:21:38
+  Created on July 22, 2022 20:47:56
 
   $ python3 api_logic_server_run.py [Listener-IP] [port] [swagger-IP]    # starts your ApiLogicServer project
 
@@ -26,7 +26,7 @@ handler.setFormatter(formatter)
 app_logger.addHandler(handler)
 app_logger.propagate = True
 
-app_logger.setLevel(logging.DEBUG)  # log levels: critical < error < warning(20) < info(30) < debug
+app_logger.setLevel(logging.INFO)  # log levels: critical < error < warning(20) < info(30) < debug
 app_logger.info(f'\nAPI Logic Project Starting: {__file__}')
 
 logging.getLogger('safrs').setLevel(logging.INFO)
@@ -127,13 +127,19 @@ def flask_events(flask_app):
 
     @flask_app.route('/ui/admin/admin.yaml')
     def admin_yaml():
+        """
+        returns response of admin.yaml
+        and text-substitutes to get url args from startup args (vs. specify them twice)
+            api_root: {http_type}://{swagger_host}:{swagger_port} (from ui_admin_creator)
+        """
         import io
         use_type = "mem"
-        if use_type == "mem":
+        if use_type == "mem":  # FIXME swagger_port http_type
             with open("ui/admin/admin.yaml", "r") as f:
                 content = f.read()
+            content = content.replace("{http_type}", http_type)
             content = content.replace("{swagger_host}", swagger_host)
-            content = content.replace("{port}", port)  # note - codespaces requires 443 here (typically via args)
+            content = content.replace("{port}", swagger_port)  # note - codespaces requires 443 here (typically via args)
             content = content.replace("{api}", API_PREFIX[1:])
             app_logger.debug(f'loading ui/admin/admin.yaml')
             mem = io.BytesIO(str.encode(content))
@@ -186,10 +192,10 @@ def flask_events(flask_app):
 
 def get_args():
     """
-    returns tuple: flask_host, swagger_host, port
+    returns tuple: (flask_host, swagger_host, port, swagger_port, http_type, verbose, create_and_run)
     """
 
-    global flask_host, swagger_host, port
+    global flask_host, swagger_host, port, swagger_port, http_type, verbose, create_and_run
 
     network_diagnostics = True
     hostname = socket.gethostname()
@@ -221,8 +227,14 @@ def get_args():
                                 help = f'ip address of the interface to which flask will be bound {flask_host})', 
                                 default = flask_host)
             parser.add_argument("-s", "--swagger_host", 
-                                help = f'ip address of the interface to which flask will be bound {swagger_host})',
+                                help = f'ip address clients use to access API {swagger_host})',
                                 default = swagger_host)
+            parser.add_argument("-q", "--swagger_port", 
+                                help = f'swagger port (eg, 443 for codespaces))',
+                                default = port)
+            parser.add_argument("-t", "--http_type", 
+                                help = f'ip address of the interface to which flask will be bound {swagger_host})',
+                                default = "http")
             parser.add_argument("-v", "--verbose", 
                                 help = f'set for more info',
                                 action=argparse.BooleanOptionalAction,
@@ -250,6 +262,8 @@ def get_args():
                 port = args.port
                 flask_host = args.flask_host
                 swagger_host = args.swagger_host
+                swagger_port = args.swagger_port
+                http_type = args.http_type
                 verbose = args.verbose
                 create_and_run = args.create_and_run
             else:                               # positional arguments (compatibility)
@@ -257,9 +271,9 @@ def get_args():
                 flask_host = args.flask_host_p
                 swagger_host = args.swagger_host_p
 
-    return flask_host, swagger_host, port, verbose, create_and_run
+    return flask_host, swagger_host, port, swagger_port, http_type, verbose, create_and_run
 
-def create_app(config_filename=None, swagger_host: str = None, flask_host: str = None):
+def create_app(swagger_host: str = None, swagger_port: str = None):
     """ creates flask_app, activates API and logic """
     # https://stackoverflow.com/questions/34674029/sqlalchemy-query-raises-unnecessary-warning-about-sqlite-and-decimal-how-to-spe
     import warnings
@@ -307,8 +321,9 @@ def create_app(config_filename=None, swagger_host: str = None, flask_host: str =
             """ Logs:
                 Declare   API - api/expose_api_models, URL = localhost, port = 5656
                 Customize API - api/expose_service.py, exposing custom services hello_world, add_order
+                FIXME - swagger_port (443)
             """
-            safrs_api = expose_api_models.expose_models(flask_app, swagger_host=swagger_host, PORT=443, API_PREFIX=API_PREFIX)
+            safrs_api = expose_api_models.expose_models(flask_app, swagger_host=swagger_host, PORT=swagger_port, API_PREFIX=API_PREFIX)
             customize_api.expose_services(flask_app, safrs_api, project_dir, swagger_host=swagger_host, PORT=port)  # custom services
 
             from database import customize_models
@@ -339,11 +354,14 @@ if is_docker() and flask_host == "localhost":
         app_logger.debug(f'\n==> Network Diagnostic - using docker_override for flask_host: {flask_host}')
 port = "5656"
 
-(flask_host, swagger_host, port, verbose, create_and_run) = get_args()
+swagger_port = port  # these for codespaces.  Not genned, provide values in launch config
+http_type = "http"
+
+(flask_host, swagger_host, port, swagger_port, http_type, verbose, create_and_run) = get_args()
 if verbose:
     app_logger.setLevel(logging.DEBUG)
 
-flask_app, safrs_api = create_app(flask_host = flask_host, swagger_host = swagger_host)
+flask_app, safrs_api = create_app(swagger_host = swagger_host, swagger_port = swagger_port)
 flask_events(flask_app)
 
 how_run = "(from WSGI)"
